ca3a62d6d5593bfa5497f6036ba42fe4de2f2fdc
Jack Randol <jack@Jacks-MBP.home>
2020-12-18 09:50:37 +0100

    project route post

--- routes/api/projects.js
+++ routes/api/projects.js
@@ -1,11 +1,57 @@
 const express = require('express');
 const router = express.Router();
+const Project = require('../../models/Project');
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
 
-// @route GET api/projects
-// @desc Test route to user
+// @route GET api/projects/id
+// @desc Test get specific project
 // @access Public
-router.get('/', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const project = await (
+      await Project.findOne({ id: req.params.id })
+    ).populate('project', ['title', 'description', 'date']);
+
+    if (!project) {
+      return res.status(400).json({ msg: 'There is no project here' });
+    }
+
+    res.json(project);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
   res.send('projects route');
 });
+
+// @route POST api/projects
+// @desc Create or update a project
+// @access Private
+
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('title', 'Title is required').not().isEmpty(),
+      check('description', 'Description is required').not().isEmpty(),
+      //most likely have to update this with the format I'll be using later
+      check('date', 'Date is required').isDate(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { title, description, date } = req.body;
+
+    const projFields = {};
+
+    projectFields.user = req.user.id;
+    res.send('great work');
+  }
+);
 
 module.exports = router;
