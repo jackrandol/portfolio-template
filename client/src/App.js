6682f84700752f2ee698a6246cc394b151070e39
Jack Randol <jackrandol@gmail.com>
2020-12-22 13:52:10 +0100

    login and redirect to dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Landing from './components/layout/Landing';
@@ -6,12 +6,23 @@ import Navbar from './components/layout/Navbar';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 //Redux
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
   return (
     <Provider store={store}>
       <Router>
@@ -22,6 +33,7 @@ const App = () => {
           <Switch>
             <Route exact path='/login' component={Login} />
             <Route exact path='/register' component={Register} />
+            <PrivateRoute exact path='/dashboard' component={Dashboard} />
           </Switch>
         </Fragment>
       </Router>
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,7 +1,46 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { Fragment } from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { logout } from '../../actions/auth';
 
-const Navbar = () => {
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+  const authLinks = (
+    <div>
+      <button className='navLink' onClick={logout}>
+        logout
+      </button>
+      <NavLink
+        exact
+        activeClassName='navLink--active'
+        className='navLink'
+        to={'/dashboard'}
+      >
+        dashboard
+      </NavLink>
+    </div>
+  );
+
+  const guestLinks = (
+    <div>
+      <NavLink
+        exact
+        activeClassName='navLink--active'
+        className='navLink'
+        to={'/login'}
+      >
+        login
+      </NavLink>
+      <NavLink
+        exact
+        activeClassName='navLink--active'
+        className='navLink'
+        to={'/register'}
+      >
+        Register
+      </NavLink>
+    </div>
+  );
   return (
     <div>
       <NavLink
@@ -28,24 +67,19 @@ const Navbar = () => {
       >
         About
       </NavLink>
-      <NavLink
-        exact
-        activeClassName='navLink--active'
-        className='navLink'
-        to={'/login'}
-      >
-        login
-      </NavLink>
-      <NavLink
-        exact
-        activeClassName='navLink--active'
-        className='navLink'
-        to={'/register'}
-      >
-        Register
-      </NavLink>
+      {!loading && (
+        <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+      )}
     </div>
   );
 };
 
-export default Navbar;
+Navbar.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+};
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, { logout })(Navbar);
