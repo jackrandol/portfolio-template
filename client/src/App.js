564bf00ac63fef4f69d47181691b702b5014bf36
Jack Randol <jackrandol@gmail.com>
2020-12-22 17:12:59 +0100

    added load projects

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Landing from './components/layout/Landing';
 import Navbar from './components/layout/Navbar';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Projects from './components/pages/Projects';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
@@ -13,6 +14,7 @@ import PrivateRoute from './components/routing/PrivateRoute';
 import { Provider } from 'react-redux';
 import store from './store';
 import { loadUser } from './actions/auth';
+import { loadProjects } from './actions/projects';
 import setAuthToken from './utils/setAuthToken';
 
 if (localStorage.token) {
@@ -23,6 +25,11 @@ const App = () => {
   useEffect(() => {
     store.dispatch(loadUser());
   }, []);
+
+  useEffect(() => {
+    store.dispatch(loadProjects());
+  }, []);
+
   return (
     <Provider store={store}>
       <Router>
@@ -31,6 +38,7 @@ const App = () => {
           <Route exact path='/' component={Landing}></Route>
           <Alert />
           <Switch>
+            <Route exact path='/projects' component={Projects} />
             <Route exact path='/login' component={Login} />
             <Route exact path='/register' component={Register} />
             <PrivateRoute exact path='/dashboard' component={Dashboard} />
