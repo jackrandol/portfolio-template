934eb1735d6531488d457ca02c8eac703a3675f4
Jack Randol <jackrandol@gmail.com>
2020-12-30 10:42:10 +0100

    changed photo upload size limit to 4mb

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ app.use(fileUpload());
 connectDB();
 
 //Init Middleware
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: '4mb' }));
+app.use(express.urlencoded({ limit: '4mb', extended: true }));
 app.use(cors());
 
 app.get('/', (req, res) => res.send(`API running`));
